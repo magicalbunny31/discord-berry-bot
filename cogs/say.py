e7afe8f14a18a9589d8838bd0bcec44b4fd0da3b
magicalbunny31 <63871095+magicalbunny31@users.noreply.github.com>
2021-11-22 18:41:34 +0000

    improve /say 🐾

--- cogs/say.py
+++ cogs/say.py
@@ -12,15 +12,25 @@ class say(commands.Cog):
    # slash command permissions: https://github.com/Pycord-Development/pycord/blob/master/examples/app_commands/slash_perms.py
    @commands.slash_command(name="say", description="› make me say something 💬", default_permission=False)
    @permissions.has_any_role("🦊", "Moderators", "Admins")
-   async def say(self, ctx, content: Option(str, "› what should i say? 📝"), channel: Option(discord.TextChannel, "› what channel should i say this in? 💬")):
+   async def say(self, ctx, content: Option(str, "› what should i say? 📝"), channel: Option(discord.TextChannel, "› what channel should i say this in? 💬", required=False), reply_to: Option(str, "› the id of the message to reply to in the channel 💬", name="reply-to-message-id", required=False)):
       if len(content) > 2000:
          return await ctx.respond(content="that message is too long! it can only be under 2000 characters", ephemeral=True)
 
       await ctx.defer(ephemeral=True)
 
-      await channel.send(content=content, allowed_mentions=discord.AllowedMentions(users=False))
+      ch = channel or ctx.channel
 
-      return await ctx.interaction.edit_original_message(content=f"sent message in {channel.mention}!")
+      if reply_to:
+         try:
+            message = await ch.fetch_message(int(reply_to))
+            await message.reply(content=content, allowed_mentions=discord.AllowedMentions.none())
+         except:
+            return await ctx.interaction.edit_original_message(content=f"the provided `reply-to-message-id` isn't a valid message in {ch.mention}!")
+
+      else:
+         await ch.send(content=content, allowed_mentions=discord.AllowedMentions.none())
+
+      return await ctx.interaction.edit_original_message(content=f"sent message in {ch.mention}!")
       
 
 
