2b050cbdc0ca496e7ed36e3055e361f05636add0
magicalbunny31 <dzeiun@googlemail.com>
2022-03-01 17:55:03 +0000

    more roles for /say 🐾

--- cogs/say.py
+++ cogs/say.py
@@ -11,7 +11,7 @@ class say(commands.Cog):
    # slash command options: https://github.com/Pycord-Development/pycord/blob/master/examples/app_commands/slash_options.py
    # slash command permissions: https://github.com/Pycord-Development/pycord/blob/master/examples/app_commands/slash_perms.py
    @commands.slash_command(name="say", description="make me say something 💬", default_permission=False)
-   @permissions.has_any_role("🦊", "Moderators", "Admins")
+   @permissions.has_any_role("owner 🐰🦊🐺🦌", "literal fox lord 🦊", "🦊", "Moderators", "Admins")
    async def say(self, ctx, content: Option(str, "what should i say? 💭"), channel: Option(discord.TextChannel, "what channel should i say this in? 📩", required=False), reply_to: Option(str, "the id of the message to reply to in the channel ✉️", name="reply-to-message-id", required=False)):
       if len(content) > 2000:
          return await ctx.respond(content="that message is too long! it can only be under 2000 characters", ephemeral=True)
