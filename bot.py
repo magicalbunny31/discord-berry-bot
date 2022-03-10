dccddbec874a95ad9c090c6ce999678164a321d7
magicalbunny31 <dzeiun@googlemail.com>
2022-03-10 19:08:35 +0000

    change emoji in status to 🫐🐾

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ from assets.data.strip_indents import strip_indents
 bot = discord.Bot(
    # debug_guilds = [719616264934522940],
    activity = discord.Activity(
-      name = "berry berry berry 🍓",
+      name = "berry berry berry 🫐",
       type = discord.ActivityType.watching
    ),
    status = discord.Status.idle,
